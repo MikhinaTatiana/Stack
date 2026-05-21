2cae0dbe43a6f85b97439071dbc80e7e81a4fc37
MikhinaTatiana <mikhinato@yandex.ru>
2026-05-21 16:12:38 +0300

    Update VectorStack.h

--- VectorStack.h
+++ VectorStack.h
@@ -1,1 +1,30 @@
+#pragma once
+#include "StackImplementation.h"
+#include <vector>
 
+class VectorStack : public IStackImplementation {
+public:
+    VectorStack() = default;
+    ~VectorStack() override = default;
+
+    void push(const ValueType& value) override {
+        _data.push_back(value);
+    }
+    
+    void pop() override {
+        if (!isEmpty()) {
+            _data.pop_back();
+        }
+    }
+    const ValueType& top() const override {
+        return _data.back();
+    }
+    bool isEmpty() const override {
+        return _data.empty();
+    }
+    size_t size() const override {
+        return _data.size();
+    }
+private:
+    std::vector<ValueType> _data;
+};
